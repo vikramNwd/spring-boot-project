72c84739c2b906abdcbf3f5ea4a0ffea7685b468
Vikram <vikramkumarnwd478@gmail.com>
2026-07-01 13:38:17 +0530

    Committing...

---
+++ FTA_23_Eureka_Server/src/test/java/com/eureka/Fta23EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fta23EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
